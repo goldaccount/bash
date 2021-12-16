cbdf940de5c7fe8e59f075da0a9b65711fe8f27e
Oren <oren.nemo412@gmail.com>
2021-12-17 03:59:45 +0700

    Add imgindexmobile for phone with absolute path

---
+++ shlls/imgindexmobile.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+for y in $(ls -1dv */)
+do
+	echo $y
+	echo "<html>
+<body>" > $y/001.html
+	index=""
+	pre="<img src=\""
+	suff="\" width="100%"></img>"
+	for x in $(ls -v $y*.jpg)
+	do
+		#echo ${x/*\//}
+		index=${x/*\//}
+		echo $pre$index$suff"</br>" >> $y/001.html
+	done
+	echo "</body>
+	</html>" >> $y/001.html
+done
+echo "Finished generating individual indexes.
+#Generating master index.html..."
+echo "<html>
+<body>" > index.html
+for y in $(ls -R */001.html) 
+do
+	index=""
+	pre="<a href=\""
+	suff="</a>"
+	y1=${y/\/001.html/""}
+	y2=${y1/_/" "}
+	index="/storage/70E4-577D/sys/"$y\"">"$y2
+	echo $pre$index$suff"</br>" >> index.html
+done
+echo "</body>
+</html>" >> index.html
+echo "Finished master index.html"
