e176ae1a689c4595a85027dec21ddcc6b8c4f2ce
Oren <oren.nemo412@gmail.com>
2021-12-21 01:02:00 +0700

    Added script to check hash.

---
+++ shlls/checkhash.sh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+inputfile=$2
+inputalgo=$1
+if [[ -z $1 || -z $2 ]]; then
+	echo "Syntax: checkhash <algorithm> <inputfile> (<hash string>)
+	Algorithm = md5, sha1, ...
+	Hash string from input, or assume content of <inputfile>.<algorithm>"
+	exit
+fi
+
+if [[ -z $3 ]]; then
+	inputhash=$(cat ${inputfile}.${inputalgo})
+else
+	inputhash=$3
+fi
+echo Hash"\t"$inputhash
+
+if [[ $(${inputalgo}sum $inputfile | sed -E 's/\s.*//') == $inputhash ]]; then;
+	echo Matched.
+	return 0
+	exit
+else
+	echo Mismatched.
+	return 1
+	exit
+fi
