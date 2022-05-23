7e7bee51decd3c87cd20685555607842b9830cb7
Oren <oren.nemo412@gmail.com>
2022-05-23 23:56:48 +0700

    Switch shot scripts to maim, removed dash from yttrim

--- shlls/shotall.sh
+++ shlls/shotall.sh
@@ -1,2 +1,6 @@
 #!/bin/zsh
-[[ ! -d $HOME/Pictures/shots ]] && mkdir -p $HOME/Pictures/shots || scrot $HOME/Pictures/shots/%Y%m%d_%H%M%S.png -e 'xclip -selection clipboard -t image/png -i $f' 
+date=$(date +%Y%m%d_%H%M%S)
+fullpath=$HOME/Pictures/shots/$date
+[[ ! -d $HOME/Pictures/shots ]] && mkdir -p $HOME/Pictures/shots
+#scrot -q 100 ${fullpath}.png -e 'convert $f -crop 1920x1080+0+60 $f'
+maim -g 1920x1080+0+60 -m 10 -u -f png ${fullpath}.png | xclip -selection clipboard -t image/png -i ${fullpath}.png
