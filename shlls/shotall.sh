e7c7c9e68ecac6931c340f2329ef09e8a00b2144
Oren <oren.nemo412@gmail.com>
2022-06-10 22:20:35 +0700

    Modified scripts

--- shlls/shotall.sh
+++ shlls/shotall.sh
@@ -3,4 +3,5 @@ date=$(date +%Y%m%d_%H%M%S)
 fullpath=$HOME/Pictures/shots/$date
 [[ ! -d $HOME/Pictures/shots ]] && mkdir -p $HOME/Pictures/shots
 #scrot -q 100 ${fullpath}.png -e 'convert $f -crop 1920x1080+0+60 $f'
-maim -g 1920x1080+0+60 -m 10 -u -f png ${fullpath}.png | xclip -selection clipboard -t image/png -i ${fullpath}.png
+maim -g 1920x1080+0+60 -m 10 -u -f png ${fullpath}.png
+xclip -selection clipboard -t image/png -i ${fullpath}.png
