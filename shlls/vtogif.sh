bb99b2541593198860b988187b5193984b5cb5f5
Oren <oren.nemo412@gmail.com>
2022-06-20 11:35:20 +0700

    update mobile sd card imgindex, update vtogif param

--- shlls/vtogif.sh
+++ shlls/vtogif.sh
@@ -1,8 +1,9 @@
 #!/bin/zsh
 #$1 = input
-#$2 = scale /
+#$2 = scale
+#$3 = fps
 
-ffmpeg -i $1 -vf "fps=30,scale=iw/${2}:ih/${2}:flags=lanczos,palettegen" $1.png && \
-ffmpeg -i $1 -i $1.png -filter_complex "fps=30,scale=iw/${2}:ih/${2}:flags=lanczos[x];[x][1:v]paletteuse" $1.gif
+ffmpeg -i $1 -vf "fps=${3},scale=iw/${2}:ih/${2}:flags=lanczos,palettegen" $1.png && \
+ffmpeg -i $1 -i $1.png -filter_complex "fps=${3},scale=iw/${2}:ih/${2}:flags=lanczos[x];[x][1:v]paletteuse" $1.gif
 echo Done converting
 mpv --loop $1.gif
