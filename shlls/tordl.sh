a2e23d62d4e1cc94caad41516e23cc0f506bff6b
Oren <oren.nemo412@gmail.com>
2022-08-11 15:43:38 +0700

    Added watch sensor tip, tordl script

--- shlls/tordl.sh
+++ shlls/tordl.sh
@@ -2,3 +2,7 @@
 today=$(date +%Y%m%d_%H%M%S)
 transmission-cli -ep -v -w $HOME/Ex/Torrent/ $1 &>$today.log &
 tail -F $today.log
+#!/bin/zsh
+#today=$(date +%Y%m%d%_H%M%S)
+#transmission-cli -m -w $HOME/Ex/Torrent $1 1>/tmp/$today &
+#tail -F /tmp/$today
