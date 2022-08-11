d6f93d11606ace36983db714b150eb02c9d975b8
Oren <oren.nemo412@gmail.com>
2022-08-11 15:47:05 +0700

    .

--- shlls/shotall.sh
+++ shlls/shotall.sh
@@ -4,8 +4,6 @@ fullpath=$HOME/Pictures/shots/$date
 [[ ! -d $HOME/Pictures/shots ]] && mkdir -p $HOME/Pictures/shots
 #-i $(xdotool getactivewindow)
 # scrot -q 100 ${fullpath}.png -e 'convert $f -crop 1920x1080+0+60 $f'
-<<<<<<< HEAD
 #maim -g 1920x1080+0+60 -m 10 -u -f png ${fullpath}.png
 maim -m 10 -u -f png ${fullpath}.png
->>>>>>> refs/remotes/origin/master
 xclip -selection clipboard -t image/png -i ${fullpath}.png
