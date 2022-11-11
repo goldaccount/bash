d4f3341313d5040dd307d5d1b2cb7ff23fa0c5fd
Oren <oren.nemo412@gmail.com>
2022-11-11 10:50:25 +0700

    Modified backup script

--- shlls/bk.sh
+++ shlls/bk.sh
@@ -1,8 +1,6 @@
 #!/bin/zsh
+#rsync files and folder list from sh/bklist starting from .config to input path (Must point to .config folder) e.g BackupLinux/.config
+echo "Copying .config to $1"
 today=".config/backup"$(date +%Y%m%d)".log"
-rsync -chiru --log-file=$today .config $1 \
-	--exclude=.config/BraveSoftware* \
-	--exclude=.config/transmission* \
-	--exclude=.config/Microsoft* \
-	--exclude=*Analytics* \
-#	--exclude=*GIMP*
+rsync --no-motd -chiruz --log-file=$today -F --include-from=$HOME/sh/bklist --exclude="*" .config/* $1
+echo "Finished copying .config to $1, log file =$today"
