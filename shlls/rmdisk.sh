4af94c96ba41b63afad4a1db8ccbe7d3275b502b
nr2 <nr2@nr200.home>
2022-12-26 10:42:52 +0700

    remove sleep from hdparm

--- shlls/rmdisk.sh
+++ shlls/rmdisk.sh
@@ -4,8 +4,8 @@ sudo umount ${1}*
 echo Idling $1
 sudo hdparm -y $1
 sleep 10
-echo Sleeping $1
-sudo hdparm -Y $1
-sleep 5
+#echo Sleeping $1
+#sudo hdparm -Y $1
+#sleep 5
 echo Powering off $1
-udisksctl power-off -b $1
+sudo udisksctl power-off -b $1
