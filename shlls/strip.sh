1273faafe23fd8b455c8359ae39b42a787e2626f
nr2 <nr2@nr200.home>
2022-12-28 21:47:42 +0700

    add strip meta from mp3 flac command

---
+++ shlls/strip.sh
@@ -0,0 +1,52 @@
+#!/bin/zsh
+#
+
+fx_mp3() {
+	eyeD3 \
+	--remove-all-comments \
+	--user-text-frame "www" \
+	--user-text-frame "COMMENT" \
+	--user-text-frame "download" \
+	--user-text-frame "DOWNLOAD SITE" \
+	--user-text-frame "encoder" \
+	$1
+}
+
+fx_flac() {
+	metaflac --remove-tag="COMMENT" --remove-tag="comment" --remove-tag="DOWNLOAD SITE" --remove-tag="encoder" --remove-tag="download" --remove-tag="www" \
+	$1
+}
+
+fx_proc() {
+	x="$1"
+	if [[ $x =~ .*mp3.* ]]; then
+		fx_mp3 $x
+	elif [[ $x =~ .*flac.* ]]; then
+		fx_flac $x
+	fi
+}
+
+fx_proc $1
+#for x in *.mp3
+#do
+#	eyeD3 \
+#	--remove-all-comments \
+#	--user-text-frame "www" \
+#	--user-text-frame "COMMENT" \
+#	--user-text-frame "download" \
+#	--user-text-frame "DOWNLOAD SITE" \
+#	--user-text-frame "encoder" \
+#	$x
+#done
+#else
+#fi
+#
+#if [[ ! -f `ls *.flac` ]]; then
+#for x in *.flac
+#do
+#	metaflac --remove-tag="COMMENT" --remove-tag="comment" --remove-tag="DOWNLOAD SITE" --remove-tag="encoder" --remove-tag="download" --remove-tag="www" \
+#	$x
+#done
+#else
+#fi
+#echo "Removed all tags."
