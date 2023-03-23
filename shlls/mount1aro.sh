4d581dcf921de69fe90feac0993c2386ac8ab609
nr2 <nr2@nr200.home>
2023-03-23 07:53:19 +0700

    Modifiy mount scripts

--- shlls/mount1aro.sh
+++ shlls/mount1aro.sh
@@ -1,2 +1,2 @@
 #/bin/zsh
-sudo mount -t cifs -o ro,noatime,user,users,uid=1000,credentials=/home/nr2/.config/mount/smb1 //tn1/p1a mount/p1a.ro
+sudo mount -t cifs -o ro,user,users,uid=1000,credentials=/home/nr2/.config/mount/smb1 //tn1/p1a mount/p1a.ro
---
+++ shlls/mount1d.sh
@@ -0,0 +1,2 @@
+#/bin/zsh
+sudo mount -t cifs -o rw,user,users,credentials=/home/nr2/.config/mount/nr2,uid=1000 //tn1/p1d mount/p1d
