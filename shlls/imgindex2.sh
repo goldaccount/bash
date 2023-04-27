2b793075250b220b300b049eef401317f9fad1c8
nr2 <nr2@nr200.home>
2023-04-27 21:31:48 +0700

    Split imgindex to imgindex2.sh for 001.html & imgindex3.sh for master crawl

--- shlls/imgindex2.sh
+++ shlls/imgindex2.sh
@@ -6,7 +6,7 @@ echo "<html>
 <body>" > $y/001.html
 index=""
 pre="<img src=\""
-suff="\" width=90%></img>"
+suff="\" width=100%></img>"
 for x in $(ls -v $y*.jpg)
 do
 	index=${x/*\//}
@@ -16,36 +16,4 @@ done
 echo "</body>
 </html>" >> $y/001.html
 done
-echo "Finished generating individual indexes.
-#Generating master indexpc.html & indexmb.html..."
-echo '<html>
-	<head>
-	<style>
-		html, body {
-		background: #000;
-		}	
-		a, p, li, h1, h2 {
-		color: #AAA; font-size: 3vw;
-		}
-	</style>	
-	</head>
-<body>
-<h1>
-<ol>' > indexpc.html > indexmb.html
-for y in $(ls -R */001.html) 
-do
-	index=""
-	pre="<li><a href=\""
-	suff="</a>"
-	y1=${y/\/001.html/""}
-	y2=${y1/_/" "}
-	index=$y\"">"$y2
-	indexm="/storage/DB2E-8DC5/sys/"$y\"">"$y2
-	echo ${pre}${index}${suff}"</br>" >> indexpc.html
-	echo ${pre}${indexm}${suff}"</br>" >> indexmb.html
-done
-echo '</h1>
-</ol>
-</body>
-</html>' >> indexpc.html >> indexmb.html
-echo "Finished master indexpc.html & indexmb.html"
+echo "Finished generating individual indexes."
