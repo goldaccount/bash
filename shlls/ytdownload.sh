200593abdf8c6d89f3bbc84249fed18ea6c822a7
nr2 <nr2@nr200.home>
2023-06-12 07:41:52 +0700

    Fix ytdown load

--- shlls/ytdownload.sh
+++ shlls/ytdownload.sh
@@ -11,12 +11,12 @@ Video:  [1080]=399,248,137,303,299
         [1440]=400,271,308
         [2160]=401,313,315
         Or input 'vf' to view full list of formats
-	";
+	"
 	exit
-else
-        mode=""
-        read -p "Choose mode" mode
-        echo $mode
+#else
+#        mode=""
+#        read -p "Choose mode" mode
+#        echo $mode
 fi
 }
 
@@ -31,6 +31,7 @@ elif [[ $1 =~ .*holodex.* ]]; then
 else
 	url="https://youtu.be/"$1
 fi
+echo $url
 }
 
 fx_process() {
@@ -46,14 +47,14 @@ if [ -z $2 ]; then
 	format=140
 fi
 #---Catch playlist start (if any)
-if [ -n $3 ]; then
-	playlist='--yes-playlist --playlist-start '$3
-fi
+#if [ -n $3 ]; then
+#	playlist='--yes-playlist --playlist-start '$3
+#fi
 
 #---Catch no playlist
-if [ -z $3 ]; then
-	playlist=''
-fi
+#if [ -z $3 ]; then
+#	playlist=''
+#fi
 #-----
 }
 
@@ -62,7 +63,8 @@ fi
 touch $currentdate.log
 fx_dl() {
 #Using config file
-ytdlp $url -f $format $playlist $4 --config-location $HOME/sh/ytdl_music --dowload-archive archive & | tee -a $currentdate.log
+echo ytdlp "$url" -f $format ${3} --config-location $HOME/sh/ytdl_music --download-archive archive | tee -a $currentdate.log
+ytdlp "$url" -f $format ${3} --config-location $HOME/sh/ytdl_music --download-archive archive | tee -a $currentdate.log
 
 }
 #title=$(ytdl $url --get-title --skip-download &)
@@ -70,3 +72,8 @@ ytdlp $url -f $format $playlist $4 --config-location $HOME/sh/ytdl_music --dowlo
 #touch $id && echo $title | tee -a $id
 
 #ytmusic Ns4HRGilgHw bv+ba "" "--cookies ~/ck/sora.txt -a download --download-archive list"
+
+fx_check "$1";
+fx_trim "$1";
+fx_process "$1" "$2" "$3";
+fx_dl "$1" "$2" "$3";
