6118d84cd811ffd64bdefe54e2fc7d82a1869361
nr2 <nr2@nr200.home>
2023-06-18 18:14:29 +0700

    Add mount1b nfs, screenshot monitor1 (shotmon1)

--- shlls/shotmon1.sh
+++ shlls/shotmon1.sh
@@ -4,6 +4,6 @@ fullpath=$HOME/Pictures/shots/$date
 [[ ! -d $HOME/Pictures/shots ]] && mkdir -p $HOME/Pictures/shots
 #-i $(xdotool getactivewindow)
 # scrot -q 100 ${fullpath}.png -e 'convert $f -crop 1920x1080+0+60 $f'
-maim -g 3840x1200+0+00 -m 10 -u -f png ${fullpath}.png
+maim -g 1920x1080+0+60 -m 10 -u -f png ${fullpath}.png
 #maim -m 10 -u -f png ${fullpath}.png
 xclip -selection clipboard -t image/png -i ${fullpath}.png
