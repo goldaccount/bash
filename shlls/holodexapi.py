90d864584b0ed759f4c02452d2550c3a0ec90f54
nr2 <nr2@nr200.home>
2024-03-10 12:38:27 +0700

    Add holodex API query, fix imgindex2, add iscsi mount

--- shlls/holodexapi.py
+++ shlls/holodexapi.py
@@ -1,20 +1,21 @@
 import requests
 import sys
 
+#print("Usage: <channel> <topic> <output> <offset>")
 def queryapi(_channel, _topic, _outfile, _offset): 
 	url = "https://holodex.net/api/v2/videos"
 	
-	querystring = {"channel_id":_channel,"topic":_topic,"type":"stream","status":"past","limit":"25","offset":_offset}
+	querystring = {"channel_id":_channel,"topic":_topic,"type":"stream","limit":"25","offset":_offset}
+#	querystring = {"channel_id":_channel,"topic":_topic,"limit":2}
 	
 	headers = {"X-APIKEY": "20f220c9-070f-4481-9fe3-308802140f38"}
 	
 	response = requests.request("GET", url, headers=headers, params=querystring)
 	
 	r = response.json()
-	
 	i = 1
 	while i < len(r):
-#		_outfile.write(r[i].get('id')+"\t"+r[i].get('title')+"\n")
+##		_outfile.write(r[i].get('id')+"\t"+r[i].get('title')+"\n")
 		_outfile.write('https://youtu.be/'+r[i].get('id')+"\n")
 		i += 1
 _channel = sys.argv[1]
@@ -23,4 +24,3 @@ _outfile = open(sys.argv[3],"a")
 _offset = sys.argv[4]
 queryapi(_channel, _topic, _outfile, _offset) 
 _outfile.close()
-
