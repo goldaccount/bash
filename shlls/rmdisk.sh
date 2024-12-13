c24540a56bb77a09ccaa2f61a163d1974f1e3004
nr2 <nr2@nr200.home>
2024-12-13 21:01:51 +0700

    rmdisk.sh Simplify commands
    kitty	add shortcuts
    cloudflare	add proxy config

--- shlls/rmdisk.sh
+++ shlls/rmdisk.sh
@@ -1,11 +1,7 @@
 #!/bin/zsh
 echo Unmounting $1
-sudo umount ${1}*
-#echo Idling $1
-#sudo hdparm -y $1
-#sleep 10
-#echo Sleeping $1
-sudo hdparm -Y $1
-#sleep 5
-echo Powering off $1
-sudo udisksctl power-off -b $1
+udisksctl unmount -b /dev/$1*
+#sudo umount ${1}*
+#sudo hdparm -Y $1
+#echo Powering off $1
+sudo udisksctl power-off -b /dev/$1
